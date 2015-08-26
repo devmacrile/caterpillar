945a05194181c127ae1e78976e00fc950ea83cba
U-VAL\RileyD <rileyde@umich.edu>
2015-08-26 01:08:56 -0400

    xgboost bench ~.345 rmsle

--- R/benchmark_xgboost.R
+++ R/benchmark_xgboost.R
@@ -7,7 +7,6 @@ library(data.table)
 
 source("rmsle.R")
 
-
 # load train/test datasets created by preprocess.R
 load("../data/clean/train.rdata")
 load("../data/clean/test.rdata")
@@ -28,17 +27,23 @@ for(i in 1:k) {
   cv.train.out <- output_vector[-fold]
   cv.test.out <- output_vector[fold]
   
-  bst <- bst <- xgboost(data = sparse_train[-fold,], label = cv.train.out, max.depth = 8, eta = 1, 
-                        nround = 10, nthread = 1, objective = "reg:linear", verbose = 1)
+  bst <- bst <- xgboost(data = sparse_train[-fold,], label = cv.train.out, max.depth = 10, eta = 1, 
+                        nround = 100, nthread = 1, objective = "reg:linear", verbose = 1)
   
-  preds <- predict(bst, sparse_train[fold])
+  preds <- predict(bst, sparse_train[fold, ])
+  preds[preds < 0] <- 0
   cv.test.err[i] <- rmsle(preds, cv.test.out)
-  
 }
 
+# Save model to disk
+xgb.dump(bst, "../models/201508252333-xgboost-bench.dump", with.stats=T)
 
 
 
+# create submission file by predicting on test data
+sparse_test <- sparse.model.matrix(~., data=as.data.table(test))
+preds <- predict(bst, sparse_test)
+preds[preds < 0] <- 0
+submission.df <- data.frame(test$id, preds)
+write.csv(submission.df, "../data/submissions/201508252333-submission.csv")
 
-
-  
